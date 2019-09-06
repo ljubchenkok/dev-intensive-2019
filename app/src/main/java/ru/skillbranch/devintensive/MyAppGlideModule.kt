e9a6292b8e3f8c261ede8135ec056c20ec68fecb
Konstantin Ljubchenko <ljubchenko.k@gmail.com>
2019-09-06 16:36:29 +0300

    fix Glide App error

--- app/src/main/java/ru/skillbranch/devintensive/MyAppGlideModule.kt
+++
@@ -1,8 +0,0 @@
-package ru.skillbranch.devintensive
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-
-@GlideModule
-class MyAppGlideModule : AppGlideModule()
